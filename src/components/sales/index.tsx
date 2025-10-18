0635ea2e7c9adb6d8b83d8fefadbb3939efbffe2
TonAlmeida <eltonsantos.almeida@outlook.com>
2025-10-18 15:06:30 -0300

    first commit

---
+++ src/components/sales/index.tsx
@@ -0,0 +1,168 @@
+import { CircleDollarSign, DollarSign } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Sales() {
+    return (
+        <Card className="flex-1">
+            <CardHeader>
+                <div className="flex items-center justify-center">
+                    <CardTitle className="text-lg sm:text-xl text-gray-700">
+                        Ultimos Clientes
+                    </CardTitle>
+                    <CircleDollarSign className="ml-auto w-4 h-4" />
+                </div>
+                <CardDescription>
+                    ultimos clientes nas últimas 24 horas
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent className="max-h-[400px] overflow-scroll">
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+                <article className="flex items-center gap-2 border-b py-2">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/TonAlmeida.png" />
+                        <AvatarFallback>DV</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p className="text-sm sm:text-base font-semibold">
+                            sujeito programador
+                        </p>
+                        <span className="text-[12px] sm:text-sm test-gray-400">teste@teste.com</span>
+                    </div>
+                </article>
+            </CardContent>
+        </Card>
+    )
+}
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,153 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Sheet,
+        SheetTrigger,
+        SheetContent,
+        SheetTitle
+ } from "../ui/sheet";
+import { Home, LineChart, LogOut, Package, PanelBottom, Settings, ShoppingBag, Users } from "lucide-react";
+import { TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
+import { Tooltip } from "../ui/tooltip";
+
+export default function Sidebar() {
+    return (
+        <div className="flex w-full flex-col bg-muted/40">
+
+            <aside className="hidden fixed inset-y-0 left-0 z-10 w-14 border-r bg-background sm:flex flex-col">
+                <nav className="flex flex-col items-center gap-4 px-2 py-5">
+                    <TooltipProvider>
+                        <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full">
+                            <Package className="h-4 w-4" />
+                            <span className="sr-only">Dashbord Avatar</span>
+                        </Link>
+
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Home className="h-5 w-5" />
+                                    <span className="sr-only">início</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">início</p>
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <ShoppingBag className="h-5 w-5" />
+                                    <span className="sr-only">pedidos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">pedidos</p>
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Package className="h-5 w-5" />
+                                    <span className="sr-only">produtos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">produtos</p>
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Users className="h-5 w-5" />
+                                    <span className="sr-only">clientes</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">clientes</p>
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Settings className="h-5 w-5" />
+                                    <span className="sr-only">configurações</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">configurações</p>
+                            </TooltipContent>
+                        </Tooltip>
+
+                    </TooltipProvider>
+                </nav>
+                <nav className="flex flex-col items-center gap-4 px-2 py-5 h-full">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild className="mt-auto flex">
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <LogOut className="h-5 w-5 text-red-500" />
+                                    <span className="sr-only">sair</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right" >
+                                <p className="px-3 border rounded-sm bg-white">sair</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </nav>
+            </aside>
+
+            <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+                    <Sheet>
+                        <SheetTrigger asChild >
+                            <Button size="icon" variant="outline" className="sm:hidden" >
+                                <PanelBottom className="h-5 w-5" />
+                                <span className="sr-only">Abir/fechar menu</span>
+                            </Button>
+                        </SheetTrigger>
+
+                        <SheetContent side="left" className="sm:max-w-xs">
+                            <SheetTitle className="sr-only">conteúdo</SheetTitle>
+                            <nav className="grid gap-6 text-lg font-medium">
+                                <Link href="#" className="ml-5 mt-5 flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-white md:text-base gap-2">
+                                    <Package className="h-5 w-5 transition-all"/>
+                                    <span className="sr-only">logo do projeto</span>
+                                </Link>
+
+                                <Link href="#" className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
+                                    <Home className="h-5 w-5 transition-all"/>
+                                    <span>início</span>
+                                </Link>
+
+                                <Link href="#" className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
+                                    <ShoppingBag className="h-5 w-5 transition-all"/>
+                                    <span>pedidos</span>
+                                </Link>
+
+                                <Link href="#" className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
+                                    <Package className="h-5 w-5 transition-all"/>
+                                    <span>produtos</span>
+                                </Link>
+
+                                <Link href="#" className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
+                                    <Users className="h-5 w-5 transition-all"/>
+                                    <span>clientes</span>
+                                </Link>
+
+                                <Link href="#" className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
+                                    <Settings className="h-5 w-5 transition-all"/>
+                                    <span>configurações</span>
+                                </Link>
+                            </nav>
+                        </SheetContent>
+                    </Sheet>
+                    <h2>Menu</h2>
+                </header>
+            </div>
+        </div>
+    )
+}
