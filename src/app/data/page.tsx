da7d02fbdb9e4d1f86da5b0e87d883e09f2129fa
TonAlmeida <eltonsansantos.almeida01@gmail.com>
2025-11-21 17:36:35 -0300

    fixing delete all data

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -83,6 +83,7 @@ export default function Data() {
 
       const deleteAllData = () => {
         setConfirmDelection(true);
+        setTransactionsData([]);
         setTimeout(() => {
           setConfirmDelection(false);
         }, 3000);
